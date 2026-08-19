d7b48d7b357aae630a40210418c5068ab36a244c
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-08-19 11:18:16 -0300

    add - estrutura html para a home page

--- index.php
+++ index.php
@@ -6,3 +6,28 @@ $usuarios = mysqli_query($conexao, "SELECT * FROM usuarios");
 $pratos = mysqli_query($conexao, "SELECT * FROM pratos");
 
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante Larica Now</title>
+</head>
+
+<body>
+    <header>
+        <h1>CRUD - Restaurante Larica Now</h1>
+    </header>
+
+    
+
+
+
+
+
+</body>
+
+</html>
